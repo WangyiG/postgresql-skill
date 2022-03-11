694c81a0f558f06ba3dca2c1c1a2a8d54381bbe3
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-03-11 14:41:59 +0800

    偏移

--- 辅助用序列.sql
+++ 辅助用序列.sql
@@ -15,3 +15,11 @@
 3.first_value(),last_value(),nth_value() 聚合函数,需要搭配over使用，不使用order by 则基于原始行取值，有点fillna(nth_value)的感觉
 
 - select *,nth_value(v,2) over (partition by id) from (values(1,3),(1,1),(1,2),(2,5),(2,6),(2,4)) as x(id,v)
+
+4.lead(col_name,step,fillna_value)向上偏移,lag(col_name,step,fillna_value)向下偏移,这2个偏移函数需要配合over()使用，其中partition无时以整个表做为分区
+
+- select *,lead(v,1,0) over (partition by id) from (values(1,3),(1,1),(1,2),(2,5),(2,6),(2,4)) as x(id,v)
+
+- select *,lag(v,1,0) over (partition by id) from (values(1,3),(1,1),(1,2),(2,5),(2,6),(2,4)) as x(id,v)
+
+- select *,lead(v,1,0) over () from (values(1,3),(1,1),(1,2),(2,5),(2,6),(2,4)) as x(id,v)
