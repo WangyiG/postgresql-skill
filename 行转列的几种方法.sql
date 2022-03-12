6de22aa63721e1a1ea4ddc85194d05b7e2064c40
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-03-12 08:56:43 +0800

    Update 行转列的几种方法.sql

--- 行转列的几种方法.sql
+++ 行转列的几种方法.sql
@@ -28,8 +28,16 @@ from
 -- crosstab的安装：终端进入psql，在有create权限的用户下（su mt，输入用户密码postgres），执行create extension tablefunc;
 --需要注意的是这里的old_table使用cte报错了，可能是不支持临时表，另外new_table中的数据类型必须准确，否则也会报错
 /*
-crosstab函数为具有相同row_name值的每个连续输入行组生成一个输出行。使用value这些行中的字段从左到右填充输出列。如果组中的行少于输出value列，则额外的输出列将填充空值; 如果有更多行，则跳过额外的输入行，实际上，SQL查询应始终指定ORDER BY 1,2(这里的1，2是相对位置，select选中的第几个)以确保输入行的顺序正确，即具有相同值的值row_name汇集在一起​​并在行中正确排序。类似pivot(name,info,value)
+1.crosstab函数为具有相同row_name值的每个连续输入行组生成一个输出行。使用value这些行中的字段从左到右填充输出列。如果组中的行少于输出value列，则额外的输出列将填充空值; 
+如果有更多行，则跳过额外的输入行，实际上，SQL查询应始终指定ORDER BY 1,2(这里的1，2是相对位置，select选中的第几个)以确保输入行的顺序正确，即具有相同值的值row_name汇
+集在一起并在行中正确排序。类似pivot(name,info,value)
+2.当有部分缺失时,crosstab不会聪明的对齐,使用第二个参数$$values('字段1'),('字段2')$$,这样才能做到对齐
 */
 SELECT *
-FROM crosstab('select name, info, value from old_table order by 1,2')
+FROM crosstab('select name, info, value from old_table order by 1,2',$$values('年龄'),('身高')$$)
 AS new_table(name varchar, 年龄 int, 身高 int)
+
+SELECT * 
+FROM crosstab ('select sid,cid,score from sc order by 1,2',$$values('01'::text),('02'::text),('03'::text)$$ ) 
+AS x(sid varchar,a numeric,b numeric,c numeric)
+
