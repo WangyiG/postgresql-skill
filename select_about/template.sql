d22cb8c951b80b7b6187e6ac27a6b38cf13491d5
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-04-05 14:47:17 +0800

    Update template.sql

--- select_about/template.sql
+++ select_about/template.sql
@@ -2,13 +2,13 @@
 brew install postgresql@14
 
 -- 查看版本,确认已安装
- pg_ctl -V
+pg_ctl -V
 
 -- 初始化数据库
 /*
-指定的数据目录:/Users/mt/other/postgresql
-指定管理员:-U mt
-指定字符集: -E UTF8
+指定的数据目录(linux下的$PGDATA):-D,此选项指定应该存储数据库集群的目录,必传.也可以设置环境变量PGDATA来替换-D选项.
+指定超级用户:-U,默认为运行initdb的电脑用户的名称.
+指定字符集: -E UTF8,选择模板数据库的编码,这也是稍后创建任何数据库的默认编码
 指定字符排序:--lc-collate=C
 指定字符分类:--lc-ctype=en_US.UTF-8
 指定消息语言:--lc-messages=en_US.UTF-8
@@ -18,3 +18,16 @@ brew install postgresql@14
 initdb -D /Users/mt/other/postgresql -U mt --lc-collate=C --lc-ctype=en_US.UTF-8 --lc-messages=en_US.UTF-8 -E UTF8
 
 -- 启动数据库
+pg_ctl -D /Users/mt/other/postgresql  start
+
+-- 生成默认数据库postgres与模版数据库template0,template1
+createdb
+
+-- 执行完以上所有操作后才可以在终端使用psql命令
+
+-- 关于模版数据库template0与template1
+/*
+1.首先要牢记,后续所有create database db_name [template][template_name]建库都是默认基于模版库template1创建的,而template1的设置基于初始化initdb
+2.template0与template1的区别:
+template0不能被连接,template1可以被连接并创建对象(表,视图,函数等),并�且
+*/
