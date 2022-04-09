ea7272a8c70d2f26ed7b12ef50f4e0a86ebb6fc9
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-04-09 08:59:14 +0800

    Create 矩阵路径及最小路径和动归.py

---
+++ python/矩阵路径及最小路径和动归.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# 数据准备
+arr = np.arange(25).reshape(5,-1)
+brr = np.vstack((arr,arr.T))
+m2 = np.vstack((brr[:,:2],brr[:,1:3],brr[:,2:4],brr[:,3:5])) 
+df = pd.DataFrame(m2,columns=['a','b'])
+
+
+G = nx.from_pandas_edgelist(df,source='a',target='b')
+
+# 设置布局
+pos = nx.kamada_kawai_layout(G)
+
+# 图像细节设置
+nx.draw(G,pos,with_labels=True,node_color=range(25),node_size=400,edge_color=range(df.shape[0]),width=1,alpha=0.6,
+        arrowstyle="->",arrowsize=10,font_size=14, font_family="sans-serif")
+
+#nx.draw(G,pos,with_labels=True)
+plt.show()
+
+# 所需路径
+list(nx.all_simple_paths(G,0,24,cutoff=8))[:10],len(list(nx.all_simple_paths(G,0,24,cutoff=8)))
+
+# 动态规划求最小路径和
+def min_sum(arr):
+    m,n= arr.shape[0],arr.shape[1]
+    opt = np.zeros((m,n))
+    for i in range(m):
+        for j in range(n):
+            if i==j==0:
+                opt[i][j] = 0
+            elif i==0:
+                opt[i][j] = opt[i][j-1]+arr[i][j]
+            elif j==0:
+                opt[i][j] = opt[i-1][j]+arr[i][j]
+            else:
+                opt[i][j] = min(opt[i-1][j],opt[i][j-1])+arr[i][j]
+    return opt
+min_sum(arr)
+
+
+
