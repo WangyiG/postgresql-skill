27caac6b42895db8bce062345395758a9e9dc1dd
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-04-12 10:48:43 +0800

    Update n日留存.py

--- python/n日留存.py
+++ python/n日留存.py
@@ -1,3 +1,4 @@
+# pd实现
 df = pd.read_excel('/Users/mt/Desktop/新增与留存.xlsx')
 
 (df.assign(首登=df.groupby('用户ID').日期.transform('first'),
@@ -9,3 +10,30 @@ df = pd.read_excel('/Users/mt/Desktop/新增与留存.xlsx')
    .用户ID.nunique()
    .unstack()
    .fillna('')).iloc[:,[4,0,1,2,3]]
+
+# sql实现
+select 
+      首登,
+      max(num) filter(where 留存=0) 首日新增,
+      max(num) filter(where 留存=1) 第1日留存,
+      max(num) filter(where 留存=2) 第2日留存,
+      max(num) filter(where 留存=3) 第3日留存,
+      max(num) filter(where 留存=4) 第4日留存
+from 
+     (select 
+           首登,
+     	留存,
+     	count(distinct id) num 
+       from
+           (select 
+         	      *,
+         	      first_value(date) over w 首登,
+         	      date-first_value(date) over w 留存,
+         	      row_number() over w -1 连续 
+             from 
+         	      load_user window w as (partition by id order by date)) t
+       where 留存 = 连续
+       group by 首登,留存) m
+group by 首登
+
+
