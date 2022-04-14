fd31c7ed767f67b633514cc4500c3e17bf3d7d15
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-04-14 23:52:12 +0800

    Update 正则匹配.py

--- python/正则匹配.py
+++ python/正则匹配.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
-df = pd.DataFrame({'id':[1,2],'value':['skls/32c,Msg/sms1,sms2,sms3,SKG/CD,FS','lv32/MDF,ddl/kds,dsf,dsf,bee/lpk,css']})
-
 '''
 1.?P<group_name>,命名组
 2.+，*接？表示非贪婪模式，尽可能少的匹配
 3.(?<=pattern)前向界定,前向否定(?<!pattern)，注意前向中不可使用\d等正则表达式
 4.(?=pattern)后向界定,后向否定(?！pattern)
 5.^首，$尾
+6.无捕获组(?:pattern),无捕获组的使用偏向把确定能匹配的放前面,不确定的放无捕获组内,如题3对比
 '''
+
+# /前面的为1组开头,后面直至另一组开头为1组
+df = pd.DataFrame({'id':[1,2],'value':['skls/32c,Msg/sms1,sms2,sms3,SKG/CD,FS','lv32/MDF,ddl/kds,dsf,dsf,bee/lpk,css']})
 df.set_index('id').value.str.extractall('(?P<阿强>\w+?(?=/))/(?P<阿珍>.+?(?=,\w+/|$))')
 
 
@@ -17,3 +19,7 @@ s = pd.Series(['02.07.2021 - 07.07.2021, 04.08.2021, 19.06.2021 - 21.06.2021','1
 s.str.extractall('([\d\.]{10})[\s-]*([\d\.]+)*(?=,|$)')
 
 
+# 有2个匹配的切分出来,不足部分再单独切分出来
+s = pd.Series(['add tss czz qty', 'fss esc mdk', 'ssl .str.extractall('(\w+\s\w+|\w+$)').droplevel(1)mny tc fkl dek', 'mq'])
+s.str.extractall('(\w+(?:\s\w+)?)').droplevel(1)
+s.str.extractall('(\w+\s\w+|\w+$)').droplevel(1)
