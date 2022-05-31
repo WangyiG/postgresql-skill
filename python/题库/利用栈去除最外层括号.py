ca3a02aeef240c744ddbc66640b10bb50273d607
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-05-31 13:19:32 +0800

    Update 利用栈去除最外层括号.py

--- python/题库/利用栈去除最外层括号.py
+++ python/题库/利用栈去除最外层括号.py
@@ -1,17 +1,18 @@
 def f(x):
     a,res = [],''
-    def g():
-        nonlocal a,res
-        for i in x:
-            if i==')':
-                a.pop()
-                if a:
-                    res += i
-            else:
-                if a:
-                    res += i
-                a.append(i)
-        return res
-    return g()
+    for i in x:
+        if i==')':
+            a.pop()
+            if a:
+                res+=')'
+        else:
+            if a:
+                res+='('
+            a.append(i)
+    return res
+
 
 pd.DataFrame(['(())','(()()(()))','(()())(())','(()())(())(()(()))()'],columns=['a']).assign(b=lambda x:x.a.map(f))
+
+
+
