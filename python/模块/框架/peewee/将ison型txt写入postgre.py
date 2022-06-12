3302d1d124097e30a7480057c7394f1c976b2337
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-13 00:02:14 +0800

    Update 将ison型txt写入postgre.py

--- python/模块/框架/peewee/将ison型txt写入postgre.py
+++ python/模块/框架/peewee/将ison型txt写入postgre.py
@@ -67,12 +67,40 @@ data_list[0]
 # 批量插入
 AnotherNotes().insert_many(data_list).execute()
 
+
+# 3.使用扩展,扩展不必自己创建jsonfield
+from playhouse.postgres_ext import *
+db = PostgresqlExtDatabase('peewee_test', user='mt', password='postgres',host='127.0.0.1') 
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Record2(BaseModel):
+    json_data = BinaryJSONField(index=True,index_type='GIN')
+    
+db.create_tables([Record2])
+
+import pathlib as pl
+import orjson
+path = pl.Path('/Users/mt/Documents/python/群友题解/IO数据/records/record_2.txt')
+with path.open('r',encoding='utf-8') as fr:
+    data_list = []
+    for i in fr:
+        data_list.append([orjson.loads(i)])
+data_list[0]
+
+with db.atomic():
+    Record2().insert_many(data_list,fields = [Record2.json_data]).execute()
+
+
+
 # 总结：
 ‘’‘
 新建字段类，根据field_type,指定数据库以jsonb格式解析
 对于insert_many，2次data_list的区别：
 第一个data_list是一个二维列表,列表元素为:"{xxx:xxx,xx:xx}"字符串,只包含数据内容，需要指定fields参数，注意该参数是个列表形式，fields=[x,xx]
 第二个data_list是一个一维列表，列表元素为:{'name':xx,'timestamp':xx,'content':xx},一一对应了field name，timestamp，content及其数据内容
+在postgre中查找那么包含张三:SELECT * FROM record2 WHERE json_data @>'{"name":"张三"}'
 ’‘’
 
 
