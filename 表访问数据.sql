6b64ecdaa5e7ec7a9717e36be7380bbd39064736
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-14 11:37:55 +0800

    Update 表访问数据.sql

--- 表访问数据.sql
+++ 表访问数据.sql
@@ -13,3 +13,16 @@
 3.行表达式
 
 -- select * from (values(1,'test1'),(2,'test2'),(3,'test3')) as t(id, info)
+
+
+-- union 去重，需要注意的是去重不仅是去除2表中都有的重复行，单表中的重复行也会被去除成非重
+select * from (values (1),(1)) x(id) union select * from (values (2),(2)) y(id);
+
+-- union all 不去重
+select * from (values (1),(2)) x(id) union select * from (values (2),(3)) y(id);
+
+-- 做差,前表与后表做差，结果返回前表减去交集，并去重，做差的顺序会影响结果
+select * from (values (1),(1),(2)) x(id) except select * from (values (2),(2)) y(id);
+
+-- 取2表交集，结果会去重
+select * from (values (1),(1),(2)) x(id) intersect select * from (values (2),(2)) y(id);
