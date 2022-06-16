14e2d7b6398fd1aefd78ae7b782ab38f34b1758a
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-16 11:07:48 +0800

    Update 存储过程示例.sql

--- select_about/存储过程示例.sql
+++ select_about/存储过程示例.sql
@@ -19,3 +19,32 @@ LANGUAGE plpgsql;
 
 
 select m,myfunc(m) from generate_series(1,10) as m
+
+
+-- 2.定义一个接收array参数的mysum存储过程，再把列array_agg转成array，给mysum去调用
+CREATE TABLE foo (fooid INT, foosubid INT, fooname TEXT);  
+INSERT INTO foo VALUES (1, 2, 'three');  
+INSERT INTO foo VALUES (4, 5, 'six'); 
+
+
+CREATE FUNCTION mysum(int[]) RETURNS int8 AS $$  
+DECLARE  
+  s int8 := 0;  
+  x int;  
+BEGIN  
+  FOREACH x IN ARRAY $1  
+  LOOP  
+    s := s + x;  
+  END LOOP;  
+  RETURN s;  
+END;  
+$$ LANGUAGE plpgsql;
+
+SELECT mysum(ARRAY_AGG(fooid)) from foo;
+
+
+
+
+
+
+
