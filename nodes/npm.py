fd3e85fa263237c805b0783e240a538484ebf0c7
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-20 10:48:50 +0800

    Update npm.py

--- nodes/npm.py
+++ nodes/npm.py
@@ -1,6 +1,7 @@
 # win下全局问题
 # 先将安装路径下的npm.cmd用vscode打开,将其中的prefix -g修改为prefix --location=global
 npm install -g npm
+https://blog.csdn.net/qq_48485223/article/details/122709354
 
 # WARN logfile could not be created
 将c盘当前用户下的隐藏文件.npmrc删除
