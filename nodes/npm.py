5c527ce98c863b3ccb70911d2937407329d70006
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-23 08:21:31 +0800

    Update npm.py

--- nodes/npm.py
+++ nodes/npm.py
@@ -2,6 +2,8 @@
 # 先将安装路径下的npm.cmd用vscode打开,将其中的prefix -g修改为prefix --location=global
 npm install -g npm
 https://blog.csdn.net/qq_48485223/article/details/122709354
+# log问题
+在安装目录设置node_chache,node_global属性-安全-打开用户完全控制
 
 # WARN logfile could not be created
 将c盘当前用户下的隐藏文件.npmrc删除
