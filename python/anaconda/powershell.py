5f01a20893353be722ec406a7d2eb2ec14c06145
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-28 22:16:27 +0800

    Update powershell.py

--- python/anaconda/powershell.py
+++ python/anaconda/powershell.py
@@ -3,6 +3,7 @@ conda init powershell
 
 // 查看当前虚拟环境列表
 conda env list
+conda info -e
 
 // 创建虚拟环境
 sudo conda create -n drf python=3.8
@@ -13,7 +14,7 @@ conda activate drf
 conda deactivate drf
 
 // 删除指定虚拟环境
-conda remove -n drf --all
+sudo conda remove -n drf --all
 
 // 查看当前环境中手动安装的包
 pip freeze
