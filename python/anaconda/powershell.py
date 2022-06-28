b7951b36467c714245d6616a3bf3f0997caec2db
WangyiG <72795595+WangyiG@users.noreply.github.com>
2022-06-28 21:51:26 +0800

    Update powershell.py

--- python/anaconda/powershell.py
+++ python/anaconda/powershell.py
@@ -1,10 +1,16 @@
 # D:\Python\anaconda\Scripts 把Scripts目录路径添加至系统变量
 conda init powershell
 
+// 查看当前虚拟环境列表
 conda env list
 
+// 创建虚拟环境
 sudo conda create -n drf python=3.8
 
+// 切换活跃环境
 conda activate drf
 
 conda deactivate drf
+
+// 删除指定虚拟环境
+conda remove -n drf --all
